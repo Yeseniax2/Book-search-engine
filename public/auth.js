59fd14650ad066e9803cb48265b008e38e843fab
yesenia <yeseniab743@gmail.com>
2023-12-14 12:15:50 -0500

    added mutation

--- public/auth.js
+++ public/auth.js
@@ -24,3 +24,14 @@ class Authservice {
 getToken(){
     return localStorage.getItem('id_token');
 }
+
+login(idToken) {
+    localStorage.setItem('id_token, idToken');
+    window.location.assign('/');
+}
+logout(){
+
+localStorage.removeItem('id_token');
+window.location.assign('/');
+}
+export default new Authservice();
